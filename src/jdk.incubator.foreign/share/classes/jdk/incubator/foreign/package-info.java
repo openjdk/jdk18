aa74d836e990be4e11c04a5dfa98b6387dc29d54
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2021-12-13 21:31:59 +0000

    Fix capitalization in package-info

--- src/jdk.incubator.foreign/share/classes/jdk/incubator/foreign/package-info.java
+++ src/jdk.incubator.foreign/share/classes/jdk/incubator/foreign/package-info.java
@@ -139,7 +139,7 @@
  *
  * When a memory segment is created from Java code, the segment properties (spatial bounds, temporal bounds and confinement)
  * are fully known at segment creation. But when interacting with native libraries, clients will often receive <em>raw</em> pointers.
- * such pointers have no spatial bounds (does the C type {@code char*} refer to a single {@code char} value,
+ * Such pointers have no spatial bounds (does the C type {@code char*} refer to a single {@code char} value,
  * or an array of {@code char} values, of given size?). Nor do they have any notion of temporal bounds or thread-confinement.
  * <p>
  * Raw pointers are modelled using the {@link jdk.incubator.foreign.MemoryAddress} class. When clients receive a
