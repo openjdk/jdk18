278a3ac82930bf20548b06490f5281575f69f911
Aleksey Shipilev <shade@redhat.com>
2022-01-04 07:53:22 +0100

    Metadata updates

--- test/jdk/java/security/Provider/CaseSensitiveServices.java
+++ test/jdk/java/security/Provider/CaseSensitiveServices.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2004, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 5097015 8130181
+ * @bug 5097015 8130181 8279222
  * @summary make sure we correctly treat Provider string entries as case insensitive
  * @author Andreas Sterbenz
  */
